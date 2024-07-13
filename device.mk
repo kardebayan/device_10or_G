d361d2c868cc83bea377a73a8f75decb04cd648d
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-13 23:22:02 +0530

    G: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5

--- device.mk
+++ device.mk
@@ -133,9 +133,7 @@ PRODUCT_PACKAGES += \
     hwcomposer.qcom
 
 PRODUCT_PACKAGES += \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    memtrack.msm8953
+    vendor.qti.hardware.memtrack-service
 
 PRODUCT_PACKAGES += \
     libqdMetaData.system \
