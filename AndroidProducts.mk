f1855c1a240fc8f33edfc62e12b37c4cbf3cfd3b
Debayan Kar <kardebayan3@gmail.com>
2025-09-11 11:50:07 +0530

    G: Adapt for crDroid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,8 @@
 #
-# Copyright (C) 2021-2023 The PixelOS Project
+# Copyright (C) 2021-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_G.mk
-
-COMMON_LUNCH_CHOICES := \
-    aosp_G-user \
-    aosp_G-userdebug \
-    aosp_G-eng
+    $(LOCAL_DIR)/lineage_G.mk
